ee4ef561c079ea7e9af5db5e59c58144e49cb6c1
nmhummel <natalie.hummel@gmail.com>
2021-03-17 10:41:42 -0500

    fixed rooms controller issues with instance variables

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,7 +10,7 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   def show
     #room = Room.find(params[:id])
-    render json: room
+    render json: @room
   end
 
   # POST /rooms
@@ -26,22 +26,22 @@ class RoomsController < ApplicationController
 
   # PATCH/PUT /rooms/1
   def update
-    if room.update(room_params)
-      render json: room.to_json(except: [:created_at, :updated_at]) 
+    if @room.update(room_params)
+      render json: @room.to_json(except: [:created_at, :updated_at]) 
     else
-      render json: room.errors, status: :unprocessable_entity
+      render json: @room.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /rooms/1
   def destroy
-    room.destroy
+    @room.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      room = Room.find(params[:id])
+      @room = Room.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
